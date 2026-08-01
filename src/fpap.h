f47590f39806e55c40261983f15d74d0f6e5b9ae
lilly-tau <lilly.st.claire@protonmail.com>
2026-08-02 00:21:41 +1000

    Added type stuff

--- src/fpap.h
+++ src/fpap.h
@@ -2,40 +2,37 @@
 #define X__FPAP_H__X
 #include <limits.h>
 
-/* define FPAP_types*/
+#if !__STDC__
+#	error "This is not a standard C compiler"
+#endif
+
+/* define FPAP_types */
 typedef unsigned char FPAP_BYTE;
-#if USHRT_MAX >= (UCHAR_MAX * UCHAR_MAX)
-	typedef unsigned short FPAP_WORD;
-#elif UINT_MAX >= (UCHAR_MAX * UCHAR_MAX)
-	typedef unsigned int FPAP_WORD;
-#elif ULONG_MAX >= (UCHAR_MAX * UCHAR_MAX)
-	typedef unsigned long FPAP_WORD;
-#endif
-#if USHRT_MAX >= (UCHAR_MAX * UCHAR_MAX * UCHAR_MAX * UCHAR_MAX)
-	typedef unsigned short FPAP_DWORD;
-#elif UINT_MAX >= (UCHAR_MAX * UCHAR_MAX * UCHAR_MAX * UCHAR_MAX)
-	typedef unsigned int FPAP_DWORD;
-#elif ULONG_MAX >= (UCHAR_MAX * UCHAR_MAX * UCHAR_MAX * UCHAR_MAX)
-	typedef unsigned long FPAP_DWORD;
-#endif
+typedef unsigned short FPAP_WORD;
+typedef unsigned long FPAP_DWORD;
+
+#define FPAP_BYTE_MAX UCHAR_MAX
+#define FPAP_WORD_MAX USHRT_MAX
+#define FPAP_DWORD_MAX ULONG_MAX
+
 typedef FPAP_WORD FPAP_COORD;
 typedef FPAP_DWORD FPAP_COLOR;
 typedef FPAP_BYTE FPAP_BOOL;
-typedef enum {
-	FPAP_PROPERTY_NULL,
-	FPAP_PROPERTY_INPUT,
-	FPAP_PROPERTY_INPUT_POP,
-	FPAP_PROPERTY_VSYNC,
-	FPAP_PROPERTY_ROTATION
-} FPAP_PROPERTY;
-typedef enum {
-	FPAP_SUCCESS,
-	FPAP_NO_WINDOW_SYSTEM,
-	FPAP_NO_WINDOW,
-	FPAP_WINDOW_CLOSED,
-	FPAP_UNDEFINED_PROPERTY,
-	FPAP_INACCESSIBLE
-} FPAP_ERROR;
+enum {
+	FPAP_PROPERTY_NULL	= 0x00,
+	FPAP_PROPERTY_INPUT	= 0x01,
+	FPAP_PROPERTY_INPUT_POP	= 0x02,
+	FPAP_PROPERTY_VSYNC	= 0x03,
+	FPAP_PROPERTY_ROTATION	= 0x04
+}; typedef FPAP_BYTE FPAP_PROPERTY;
+enum {
+	FPAP_SUCCESS		= 0x00,
+	FPAP_NO_WINDOW_SYSTEM	= 0x01,
+	FPAP_NO_WINDOW		= 0x02,
+	FPAP_WINDOW_CLOSED	= 0x03,
+	FPAP_UNDEFINED_PROPERTY	= 0x04,
+	FPAP_INACCESSIBLE	= 0x05
+}; typedef FPAP_BYTE FPAP_ERROR;
 typedef void *FPAP_PTR;
 typedef const void *FPAP_CONST_PTR;
 typedef FPAP_PTR FPAP_STR;
@@ -43,6 +40,23 @@ typedef FPAP_PTR FPAP;
 
 #define FPAP_TRUE 1
 #define FPAP_FALSE 0
+
+#define FPAP_BOOL_MIN FPAP_FALSE
+#define FPAP_BOOL_MAX FPAP_TRUE
+
+#define FPAP_PTR_MIN 0
+/* May be undefined behaviour */
+#define FPAP_PTR_MAX (unsigned long)((void*)-1)
+
+#define FPAP_ERROR_MIN 0
+#define FPAP_ERROR_MAX FPAP_BYTE_MAX
+#define FPAP_PROPERTY_MIN 0
+#define FPAP_PROPERTY_MAX FPAP_BYTE_MAX
+
+#define FPAP_COORD_MAX FPAP_DWORD_MAX
+#define FPAP_COLOR_MAX FPAP_DWORD_MAX
+#define CONST_PTR_MIN FPAP_PTR_MIN
+#define CONST_PTR_MAX FPAP_PTR_MAX
 
 struct fpap_ext_input_event {
 	char character;
